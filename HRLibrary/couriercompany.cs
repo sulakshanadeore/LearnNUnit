b85f04fb07989427c78e1cd8336bd5a02302afb9
deoresulakshana <deoresulakshana@gmail.com>
2024-10-15 17:50:03 +0530

    doubts

---
+++ HRLibrary/couriercompany.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HRLibrary
+{
+
+   public class Courier
+    {
+
+        public Courier(int cid,string sendername,string recname,string recadd,string senderadd,
+            float courweight,string status,int tracid,DateTime deliverydate,int userid)
+        {
+            CourierID = cid;
+            this.sendername = sendername;
+            receiverName = recname; 
+           weight = courweight;
+            senderAddress = senderadd;
+            receiverAddress = recadd;
+            this.status = status;
+            this.deliveryDate = deliverydate;
+            this.userId = userid;
+            trackingNumber = tracid;
+
+
+
+
+
+        }
+        public int CourierID { get; set; }
+        public string sendername { get; set; } 
+        public string senderAddress { get; set; }
+        public string receiverName { get; set; }
+        public string receiverAddress { get; set; }
+        public float weight { get; set; }
+
+        public string status { get; set; }
+        public int trackingNumber { get; set; }
+
+        public DateTime deliveryDate { get; set; }
+        public int userId { get; set; }
+        
+    }
+
+
+    public class couriercompany
+    {
+
+        public string CompanyName { get; set; }
+
+        public List<Courier> couriers { get; set; }
+        
+        //employeeDetails- 
+        //    collection of Employee Objects,
+        
+        //locationDetails - collection of Location Objects.
+    }
+
+    public class CourierCompanyCollection
+    {
+          public List<couriercompany> couriercompanies { get; set; }
+       // couriercompany[] couriercompanies = new couriercompany[2];
+        public CourierCompanyCollection(couriercompany company)
+        {
+            this.couriercompanies.Add(company);
+            //for (int i = 0; i < couriercompanies.Length; i++)
+            //{
+            //    this.couriercompanies[i] = company;
+            //}
+        }
+    }
+
+    interface ICourierUserService {
+        
+    string    getOrderStatus(int trackingNo);
+        /**Get the status of a courier order.
+        *@param trackingNumber The tracking number of the courier order.
+        * @return The status of the courier order (e.g., yetToTransit, In Transit, Delivered).
+*/
+
+
+    }
+
+    class CourierUserServiceCollectionImpl : ICourierUserService
+    {
+        CourierCompanyCollection _companyObj;
+
+        public CourierUserServiceCollectionImpl(CourierCompanyCollection courierCompanyCollection)
+        {
+                _companyObj = courierCompanyCollection; 
+        }
+        public string getOrderStatus(int trackingNo)
+        {
+            string courierStatus = null;
+            //   throw new NotImplementedException();
+            for(int i=0;i<=_companyObj.couriercompanies.Count;i++)
+            {
+                List<Courier> couriers=_companyObj.couriercompanies[i].couriers;
+                         
+                Courier courierdata=couriers.Find(c=>c.trackingNumber==trackingNo);
+                courierStatus=courierdata.status;
+            }
+
+            return courierStatus;
+        }
+    }
+
+
+}
--- NUnitDemo/Program.cs
+++ NUnitDemo/Program.cs
@@ -1,27 +1,98 @@
 ﻿using HRLibrary;
+using System.Xml;
 
 public class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("enter userid");
-        string userid = Console.ReadLine();
-        Console.WriteLine("enter password");
-        string password = Console.ReadLine();
-        Program p=new Program();
-        string status=p.CheckLogin(userid,password);
-        Console.WriteLine(status);
+        //Console.WriteLine("enter userid");
+        //string userid = Console.ReadLine();
+        //Console.WriteLine("enter password");
+        //string password = Console.ReadLine();
+        //Program p=new Program();
+        //string status=p.CheckLogin(userid,password);
+        //Console.WriteLine(status);
 
-        Console.WriteLine("Enter id you want to find");
-        int idToFind=Convert.ToInt32(Console.ReadLine());   
-        string data=p.FindEmpByID(idToFind);
-        Console.WriteLine(data);
+        //Console.WriteLine("Enter id you want to find");
+        //int idToFind=Convert.ToInt32(Console.ReadLine());   
+        //string data=p.FindEmpByID(idToFind);
+        //Console.WriteLine(data);
+        //    couriercompany couriercompany = new couriercompany();
+        //    couriercompany.CompanyName = "Tej";
+        //    List<Courier> couriersListForTej = new List<Courier>()
+        //    {
+        //    new Courier(1,"Amit","Sumit","Pune","Mumbai",340f,"Pending",11,new DateTime(2024,11,07),1),
+        //    new Courier(2,"Tina","Mina","Pune","Mumbai",340f,"Pending",11,new DateTime(2024,11,07),2),
+        //    new Courier(3,"Amita","Sunita","Pune","Mumbai",340f,"Pending",11,new DateTime(2024,11,07),2)
+        //    };
+        //    couriercompany.couriers = couriersListForTej;
+
+
+        //    couriercompany couriercompany1 = new couriercompany();
+        //    couriercompany.CompanyName = "Fast Track";
+        //    List<Courier> couriersListForfastTrack = new List<Courier>()
+        //    {
+        //    new Courier(1,"Amit","Sumit","Pune","Mumbai",340f,"Pending",11,new DateTime(2024,11,07),1),
+        //    new Courier(2,"Tina","Mina","Pune","Mumbai",340f,"Delivered",12,new DateTime(2024,11,07),2),
+        //    new Courier(3,"Amita","Sunita","Pune","Mumbai",340f,"Pending",11,new DateTime(2024,11,07),2)
+        //    };
+        //    couriercompany1.couriers = couriersListForfastTrack;
+
+
+
+
+        //List<CourierCompanyCollection> companyBranches = 
+        //        new List<CourierCompanyCollection>();
+        //    CourierCompanyCollection c1 = 
+        //        new CourierCompanyCollection(couriercompany);
+        //    CourierCompanyCollection c2 = 
+        //        new CourierCompanyCollection(couriercompany1);
+
+
+        //    for (int i = 0; i < couriersListForfastTrack.Count; i++) {
+        //       CourierCompanyCollection? detailsFound= companyBranches.Find(c => c.couriercompanies[i].CompanyName == "Fast Track");
+        //        if (detailsFound != null) {
+        //            detailsFound.couriercompanies[i].CompanyName = "First Flight";
+
+
+        //        }
+        //            }
+
+        string genPass = null;
+        for (int i = 0; i < 4; i++)
+        {
+             genPass += GenString();
+
+        }
+     
+
+        Console.WriteLine(genPass);
+
+
+
+
+
+
+
 
 
         Console.ReadLine();
 
     }
-
+    private static string GenString()
+    {
+        Random r = new Random();
+        Random r1 = new Random();
+        int min = r1.Next();
+        char firstchar = (char)('a' + r.Next());
+        string s = firstchar.ToString().ToLower();
+       
+        int max = r1.Next(1);
+        int num=r.Next(max,min);
+        char secondchar = (char)(r.Next(num));
+        s += secondchar.ToString().ToUpper();
+        return s;
+    }
 
     public string FindEmpByID(int id)
     { 
